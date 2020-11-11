752fe49139d7702244d180ff9115a284c177572a
Amankaium <kaium@list.ru>
2020-11-11 21:52:40 +0600

    added BaseModel and Publication model

--- publications/models.py
+++ publications/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from core.models import BaseModel
 
-# Create your models here.
+
+class Publication(BaseModel):
+    text = models.TextField(
+        null=True, blank=True,
+        verbose_name="Текст публикации"    
+    )
+
+    image = models.ImageField(
+        null=True, blank=True,
+        verbose_name="Картинка публикации"
+    )
+
+
