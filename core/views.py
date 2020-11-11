dd7d5c7c4415437fbeeaec1e0dd457143059305e
Amankaium <kaium@list.ru>
2020-11-11 20:16:44 +0600

    init pythoneast project

---
+++ core/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
+
+def homepage(request):
+    return render(request, "core/index.html")
+
+
+def authorization(request):
+    if request.method == 'POST':
+        form = request.POST
+        user_login = form["login"]
+        password = form["password"]
+        user = User.objects.get(username=user_login)
+        if user.check_password(password):
+            login(request, user)
+            return redirect(homepage)
+
+    return render(request, "core/login.html")
