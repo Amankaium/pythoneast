702237eceef8a7640be01958d04bc822de278197
Amankaium <kaium@list.ru>
2020-11-16 21:24:02 +0600

    added publication creation via function

--- publications/urls.py
+++ publications/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import all_publications
+from .views import *
 
 urlpatterns = [
     path("all/", all_publications, name='all-publications'),
+    path("<int:pk>", PublicationDetailView.as_view(), name='publication'),
+    path("create/", create_publication, name='create-publication'),
 ]
--- publications/views.py
+++ publications/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView
 from .models import Publication
+
 
 
 def all_publications(request):
     pubs = Publication.objects.all()
     return render(request, "publications/all.html", {"publications": pubs})
+
+
+class PublicationDetailView(DetailView):
+    queryset = Publication.objects.all()
+    template_name = "publications/publication.html"
+
+
+def create_publication(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        text = request.POST.get("text")
+        publication = Publication(
+            name=name,
+            text=text
+        )
+        publication.save()
+        return redirect('publication', pk=publication.pk)
+
+    return render(request, "publications/create.html")
