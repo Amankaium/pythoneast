42d4660be10ff1657ecadf3a8034812139b3ee95
Amankaium <kaium@list.ru>
2020-12-12 21:32:56 +0600

    added friends app

--- pythoneast/urls.py
+++ pythoneast/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('publication/', include("publications.urls")),
+    path('friends/', include("friends.urls"))
     path("", include("core.urls")),
 ]
 
