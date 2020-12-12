34b18ca94d920ad3c7891f6629e3f7f0c16a086f
Amankaium <kaium@list.ru>
2020-12-12 20:33:20 +0600

    installed django-bootstrap and added forms

--- publications/forms.py
+++ publications/forms.py
@@ -1,10 +1,10 @@
-from django.forms import ModelForm, CharField
+from django import forms
 from .models import Publication
 
 
-class PublicationForm(ModelForm):
-    name = CharField(max_length=10, help_text='Напишите название публикации')
-
+class PublicationForm(forms.ModelForm):
+    # name = CharField(max_length=10, help_text='Напишите название публикации')
+    # test = forms.CharField(required=True)
     class Meta:
         model = Publication
         fields = ["name", "text", "image"]
