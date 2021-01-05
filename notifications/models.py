d67ac40cffa3dab4c4f2ead87a0c4dc8da8e3cdd
Amankaium <kaium@list.ru>
2021-01-05 21:10:39 +0600

    added notifications models

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import BaseModel
+from friends.models import Invite
 
-# Create your models here.
+
+class Notification(BaseModel):
+    text = models.TextField(verbose_name="Текст уведомления")
+    is_readed = models.BooleanField(default=False, verbose_name="Прочитано ли?")
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="notification",
+        verbose_name="Пользователь",
+        null=True
+    )
+
+    def __str__(self):
+        return text[:10]
+
+
+class InviteNotification(Notification):
+    invite = models.OneToOneField(
+        to=Invite,
+        related_name="notification",
+        on_delete=models.CASCADE,
+        verbose_name="Инвайт"
+    )
+
+    
+    
+
